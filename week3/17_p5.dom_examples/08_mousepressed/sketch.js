cccc565ee348bd304889e4ae0d4886c04100f78c
Daniel Shiffman <daniel@shiffman.net>
2014-11-07 10:03:41 -0500

    week 3 examples

--- week3/17_p5.dom_examples/08_mousepressed/sketch.js
+++ week3/17_p5.dom_examples/08_mousepressed/sketch.js
@@ -1,15 +1,22 @@
-// Using HTML element specific mouse listeners, mousePressed.
+// ITP Networked Media, Fall 2014
+// DOM Manipulation
+// https://github.com/shiffman/itp-networked-media
+// Daniel Shiffman
 
-// We define the variables outside of setup so we can access them from anywhere in the sketch.
-var link;
+// These are adapted from https://github.com/lmccart/itp-creative-js
+
+// Using p5 DOM element mouse listeners, mousePressed.
+
+// Global variable for background color
 var bg;
 
 function setup() {
   createCanvas(400, 400);
   bg = color(51);
 
-  link = createA("#","Click me");
-  // Attach listeners for mouse events related to img.
+  var link = createA("#","Click me");
+  // The function changeBG becomes a callback for when the mouse is pressed
+  // On this DOM element (different than mousePressed() anywhere on page)
   link.mousePressed(changeBG);
 
 }
@@ -18,6 +25,7 @@ function draw() {
   background(bg);
 }
 
+// This function is triggered when the mouse is pressed on the link
 function changeBG() {
   bg = color(random(255));
 }
