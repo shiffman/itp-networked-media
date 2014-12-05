85aacae2e54b3ac678e2c56ee538e5a4595841a9
Daniel Shiffman <daniel@shiffman.net>
2014-12-05 09:46:40 -0500

    audio examples

---
+++ week6/01_audio/00_playsound/sketch.js
@@ -0,0 +1,26 @@
+// ITP Networked Media, Fall 2014
+// DOM Manipulation
+// https://github.com/shiffman/itp-networked-media
+// Daniel Shiffman
+
+// Based on https://github.com/shiffman/LearningProcessing
+// Thanks to Jason Sigal and p5.sound
+// https://github.com/therewasaguy/p5.sound 
+
+var song;
+
+function setup() {
+  song = loadSound('lucky_dragons_-_power_melody.mp3');
+  createCanvas(720, 200);
+  background(255,0,0);
+}
+
+function mousePressed() {
+  if ( song.isPlaying() ) { // .isPlaying() returns a boolean
+    song.stop();
+    background(255,0,0);
+  } else {
+    song.play();
+    background(0,255,0);
+  }
+}
